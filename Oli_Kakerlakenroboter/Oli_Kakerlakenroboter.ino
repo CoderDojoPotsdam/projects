b738f09ff232ae99ba88a0785cefc16ee4b9ccc3
Nicco Kunzmann <niccokunzmann@rambler.ru>
2015-01-10 17:08:27 +0100

    kakerlakenroboter

---
+++ Oli_Kakerlakenroboter/Oli_Kakerlakenroboter.ino
@@ -0,0 +1,40 @@
+/* Sweep
+ by BARRAGAN <http://barraganstudio.com> 
+ This example code is in the public domain.
+
+ modified 8 Nov 2013
+ by Scott Fitzgerald
+ http://arduino.cc/en/Tutorial/Sweep
+*/ 
+
+#include <Servo.h> 
+ 
+Servo myservo;  // create servo object to control a servo 
+                // twelve servo objects can be created on most boards
+ 
+int pos = 0;    // variable to store the servo position 
+ 
+void setup() 
+{ 
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  pinMode(13, OUTPUT);
+  pinMode(10, OUTPUT); 
+} 
+ 
+void loop() 
+{ 
+  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(10, LOW);
+  for(pos = 0; pos <= 50; pos += 2) // goes from 0 degrees to 180 degrees 
+  {                                  // in steps of 1 degree 
+    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+    delay(15);                       // waits 15ms for the servo to reach the position 
+  }
+  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(10, HIGH);
+  for(pos = 50; pos>=0; pos-=2)     // goes from 180 degrees to 0 degrees 
+  {                                
+    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+    delay(15);                       // waits 15ms for the servo to reach the position 
+  } 
+} 
