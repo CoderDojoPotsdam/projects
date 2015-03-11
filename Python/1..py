a1aee596d320e6dfa3a215b088596bb33a2a183b
Nicco Kunzmann <niccokunzmann@rambler.ru>
2015-03-11 19:15:56 +0100

    neues turtle projekt

---
+++ Python/1..py
@@ -0,0 +1,14 @@
+angle = 20
+
+turtle.left(angle)
+
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+
+turtle.left(angle)
