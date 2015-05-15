a8e1cd97cb80fe2a34956b78a4bdcd949621d125
Coder Dojo Potsdam - coderdojo@ball <coderdojopotsdam-discuss@googlegroups.com>
2015-05-15 11:47:48 +0200

    auto commit on coderdojo@ball

---
+++ Python/temperature.py
@@ -0,0 +1,26 @@
+#converts temperature Fahrenheit or Celsius
+
+def print_options():
+	print "Options"
+	print "'p' print options"
+	print "'c' convert from celsius"
+	print "'f' convert from fahrenheit"
+	print "'q' quit the program"
+
+def celsius_to_fahrenheit(c_temp):
+	return 9.0/5.0*c_temp+32
+
+def fahrenheit_to_celsius(f_temp):
+	return (f_temp-32.0)*5.0/9.0
+
+choice = "p"
+while choice != "q":
+	if choice == "c":
+		temp = float(raw_input("Celsius temperature:"))
+		print "Fahrenheit:",celsius_to_fahrenheit(temp)
+	elif choice == "f":
+		temp = float(raw_input("Fahrenheit temperature:"))
+		print "Celsius:",fahrenheit_to_celsius(temp)
+	elif choice != "q":
+		print_options()
+	choice = raw_input("option:")
