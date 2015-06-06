75a9dca1784d7d16ca2d769c1bf6785658b6ec96
Coder Dojo Potsdam - coderdojo@charon <coderdojopotsdam-discuss@googlegroups.com>
2015-06-06 22:43:01 +0200

    auto commit on coderdojo@charon

---
+++ Matthias/Sweep/Sweep.ino
@@ -0,0 +1,33 @@
+// Sweep
+// by BARRAGAN <http://barraganstudio.com> 
+// This example code is in the public domain.
+
+
+#include <Servo.h> 
+ 
+Servo myservo;  // create servo object to control a servo 
+                // a maximum of eight servo objects can be created 
+ 
+int pos = 0;    // variable to store the servo position 
+ 
+void setup() 
+{ 
+  myservo.attach(8);  // attaches the servo on pin 8 to the servo object 
+} 
+ 
+ 
+void loop() 
+{ 
+  tone (10, 440, 250);
+  for(pos = 50; pos < 150; pos += 1)  // goes from 0 degrees to 180 degrees 
+  {                                  // in steps of 1 degree 
+    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+    delay(10);  
+  } 
+  tone (10, 880, 300);
+  for(pos = 150; pos>=50; pos-=1)     // goes from 180 degrees to 0 degrees 
+  {                                
+    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+    delay(10);                       // waits 15ms for the servo to reach the position 
+  } 
+} 
---
+++ Python/Haus.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import turtle
+
+def hausdesnikolaus(g):
+  turtle.left(90)
+  turtle.forward(40*g)
+  turtle.left(90)
+  turtle.forward(30*g)
+  turtle.forward(10*g)
+  turtle.left(90)
+  turtle.forward(40*g)
+  turtle.left(90)
+  turtle.forward(40*g)
+  turtle.left(135)
+  turtle.forward(56*g)
+  turtle.right(135)
+  turtle.left(45)
+  turtle.forward(28*g)
+  turtle.right(90)
+  turtle.forward(28*g)
+  turtle.right(90)
+  turtle.forward(56*g)
+
+
+hausdesnikolaus(2)
+
+  pass
