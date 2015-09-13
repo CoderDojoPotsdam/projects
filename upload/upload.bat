4a51eab6c98cac87a478551cc3d8061cfa96c8ce
Nicco Kunzmann <niccokunzmann@rambler.ru>
2015-09-13 12:35:03 +0200

    manual commit by cheche@STRAUCH

---
+++ upload/upload.bat
@@ -0,0 +1,11 @@
+@echo off
+
+rem move to this directory
+cd %~dp0
+
+git pull origin master
+git add --all .
+git commit -m"manual commit by %USERNAME%@%COMPUTERNAME%"
+git push origin master
+
+start /max %windir%\explorer.exe https://github.com/CoderDojoPotsdam/projects
---
+++ upload/upload.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd "`dirname $0`"
+
+git add --all .
+git commit -am"manual commit `whoami`@`hostname`"
+git pull
+git push
+
+firefox https://github.com/CoderDojoPotsdam/projects &
+
