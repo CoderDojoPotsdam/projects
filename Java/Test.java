c32c3260551a0ac278342049b902ab62b8f9d343
Coder Dojo Potsdam - coderdojo@charon <coderdojopotsdam-discuss@googlegroups.com>
2016-06-18 23:18:21 +0200

    Autocommit on coderdojo@charon

---
+++ Java/Test.java
@@ -0,0 +1,19 @@
+import de.hamster.debugger.model.Territorium;import de.hamster.debugger.model.Territory;import de.hamster.model.HamsterException;import de.hamster.model.HamsterInitialisierungsException;import de.hamster.model.HamsterNichtInitialisiertException;import de.hamster.model.KachelLeerException;import de.hamster.model.MauerDaException;import de.hamster.model.MaulLeerException;import de.hamster.model.MouthEmptyException;import de.hamster.model.WallInFrontException;import de.hamster.model.TileEmptyException;public class Test extends de.hamster.debugger.model.IHamster implements de.hamster.model.HamsterProgram {public void main() {
+	while (vornFrei()) {
+		vor();
+	
+	}	   
+}
+
+void reset() {
+	linksUm();
+	linksUm();
+	linksUm();
+	linksUm();	
+}
+void rechtsUm() {
+	linksUm();
+	linksUm();
+	linksUm();
+}
+	}
---
+++ Java/testsabi.java
@@ -0,0 +1,53 @@
+import de.hamster.debugger.model.Territorium;import de.hamster.debugger.model.Territory;import de.hamster.model.HamsterException;import de.hamster.model.HamsterInitialisierungsException;import de.hamster.model.HamsterNichtInitialisiertException;import de.hamster.model.KachelLeerException;import de.hamster.model.MauerDaException;import de.hamster.model.MaulLeerException;import de.hamster.model.MouthEmptyException;import de.hamster.model.WallInFrontException;import de.hamster.model.TileEmptyException;public class testsabi extends de.hamster.debugger.model.IHamster implements de.hamster.model.HamsterProgram {public void main() {
+	wegTest();
+	/*while (vornFrei () ){ vor(); }
+
+	rechtsUm() ;*/
+    
+}
+	void rechtsUm()
+{
+	linksUm() ;
+	linksUm() ;
+	linksUm() ;	
+	}
+void laufBisWand () {
+		while (vornFrei () ){
+			vor(); 
+		}}
+void wegTest () {
+	laufBisWand ();
+	rechtsUm();
+	if (vornFrei(public void main() {
+	wegTest();
+	/*while (vornFrei () ){ vor(); }
+
+	rechtsUm() ;*/
+    
+}
+	void rechtsUm()
+{
+	linksUm() ;
+	linksUm() ;
+	linksUm() ;	
+	}
+void laufBisWand () {
+		while (vornFrei () ){
+			vor(); 
+		}}
+void wegTest () {
+	laufBisWand ();
+	rechtsUm();
+	if (vornFrei() ) {
+	laufBisWand(); 
+	}else{
+	laufBisWand();
+	if (vornFrei() ) {
+	laufBisWand(); 
+	}else{
+	laufBisWand();
+	if (vornFrei() ) {
+	laufBisWand();
+	}
+	wegTest();
+	}}}}
