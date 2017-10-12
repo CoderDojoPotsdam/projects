3db1b6a196fd0d6a8916cffff1056da8ae7c849c
Coder Dojo Potsdam - coderdojo@Apfel <coderdojopotsdam-discuss@googlegroups.com>
2017-10-12 13:46:39 +0200

    Automatic commit on Apfel

---
+++ Java/gang_transport.java
@@ -0,0 +1,8 @@
+import de.hamster.debugger.model.Territorium;import de.hamster.debugger.model.Territory;import de.hamster.model.HamsterException;import de.hamster.model.HamsterInitialisierungsException;import de.hamster.model.HamsterNichtInitialisiertException;import de.hamster.model.KachelLeerException;import de.hamster.model.MauerDaException;import de.hamster.model.MaulLeerException;import de.hamster.model.MouthEmptyException;import de.hamster.model.WallInFrontException;import de.hamster.model.TileEmptyException;public class gang_transport extends de.hamster.debugger.model.IHamster implements de.hamster.model.HamsterProgram {public void main() { 
+while(vornFrei()) vor();
+while (kornDa()) nimm();
+linksUm(); linksUm();
+while(vornFrei()) vor();
+while (!maulLeer()) gib();    
+}
+}
