6c85e18c681c0c2b41407bd121ae58b2cebcba1d
CoderDojo Potsdam - coderdojo-yacy-search-engine <coderdojo@coderdojo-yacy-search-engine>
2018-04-14 17:16:40 +0200

    manual commit coderdojo@coderdojo-yacy-search-engine

--- upload/upload.sh
+++ upload/upload.sh
@@ -7,8 +7,8 @@ git config user.email "`whoami`@`hostname`"
 git config user.name "CoderDojo Potsdam - `hostname`"
 git commit -am"manual commit `whoami`@`hostname`"
 git config push.default simple
-git pull
-git push
+sudo -H git pull
+sudo -H git push
 
 firefox https://github.com/CoderDojoPotsdam/projects &
 
