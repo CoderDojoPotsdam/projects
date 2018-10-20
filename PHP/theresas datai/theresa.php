f209f46401baf5257bca8b3bb3538d9a141c93c3
CoderDojoOS <coderdojoos-setup@gmail.com>
2018-10-20 14:02:09 +0200

    Automatic commit on coderdojo-chaos-macht-schule

---
+++ PHP/theresas datai/theresa.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>title</title>
+  </head>
+  <body><p>helloworld</p>
+    <ul>
+<?php for ($e =1; $e <101; $e++) {
+
+  echo"<li>$e.</li>";
+
+ } ?>
+
+      <p><span style="background-color:yellow;">heyy</span></p>
+
+
+
+
+
+    </ul>
+  </p>
+  <?php $hello=9; echo $hello;
+  for ($e =5; $e <10; $e++) {
+echo "$e;";
+   }
+?>
+
+
+  </body>
+</html>
