986e042623ea88376f838abd20764211f7c7f1c9
CoderDojoOS <coderdojoos-setup@gmail.com>
2020-03-02 15:19:20 +0100

    Automatic commit on turing21

--- Python/Felix.py
+++ Python/Felix.py
@@ -1,4 +1,4 @@
-Python 3.4.0 (default, Apr 11 2014, 13:05:11) 
+Python 3.6.7 (default, Apr 11 2014, 13:05:11) 
 [GCC 4.8.2] on linux
 Type "copyright", "credits" or "license()" for more information.
 >>> print("Hello World")
